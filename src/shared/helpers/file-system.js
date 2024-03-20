f08f59029e9fb535a2fc6f70c644f7e36ccd8486
stebunoff <mailbox@stebunoff.ru>
2024-03-20 11:08:32 +0300

    feat: add file reader module with helpers

---
+++ src/shared/helpers/file-system.js
@@ -0,0 +1,14 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url'
+
+export function getCurrentModuleDirectoryPath() {
+  const filePath = fileURLToPath(import.meta.url);
+  return dirname(filePath);
+}
+
+export function getFilePath(filepath) {
+  const modulePath = getCurrentModuleDirectoryPath();
+  const destination = resolve(modulePath, '../../../', filepath);
+
+  return destination;
+}
---
+++ src/shared/libs/file-reader.js
@@ -0,0 +1,23 @@
+import { existsSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
+import { getFilePath } from '../helpers/file-system.js';
+
+export async function read(filepath) {
+  const destination = getFilePath(filepath)
+  if (!existsSync(destination)) {
+    throw new Error(`File ${destination} not found.`);
+  }
+
+  let data = new Set();
+  await readFile(destination, 'utf-8')
+    .then((result) => {
+      result.split('\n')
+      .filter((row) => row.trim().length > 0)
+      .forEach((row) => data.add(row));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+    return data;
+}
